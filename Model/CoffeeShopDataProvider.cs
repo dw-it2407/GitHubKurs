4112619abbf03dd0762f801d4f1aed0bb291e564
Ditmar Burkart <ditmar.burkart@sap.com>
2025-10-17 16:30:35 +0200

    Add coffee shop model

---
+++ Model/CoffeeShopDataProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubKurs.Model
+{
+	public class CoffeeShopDataProvider
+	{
+		public IEnumerable<CoffeeShop> LoadShops()
+		{
+			return new CoffeeShop[]
+				{
+					new CoffeeShop("FT",20),
+					new CoffeeShop("Frankfurt",54),
+					new CoffeeShop("Mannheim",47),
+				};
+		}
+	}
+}
