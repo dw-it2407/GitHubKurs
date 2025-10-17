84751166619aed9caafea7463a412673cccd1840
Ditmar Burkart <ditmar.burkart@sap.com>
2025-10-17 16:36:15 +0200

    Moved coffeshop data provider to data and changed namespace

--- Data/CoffeeShopDataProvider.cs
+++ Data/CoffeeShopDataProvider.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GitHubKurs.Model;
 
-namespace GitHubKurs.Model
+namespace GitHubKurs.Data
 {
 	public class CoffeeShopDataProvider
 	{
