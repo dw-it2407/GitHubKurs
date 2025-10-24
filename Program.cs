8f6e3584d6852c55f3b87a4f2f80ad62f0576140
Ditmar Burkart <ditmar.burkart@sap.com>
2025-10-24 15:22:26 +0200

    implement shops command

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using GitHubKurs.Data;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Commands:");
 Console.WriteLine("  quit ---> exits the application");
+Console.WriteLine("   shops --->show coffee shops   ");
 
 while (true)
 {
 	string? command = Console.ReadLine();
-	if (command.Equals("quit"))
-		{ break; }
+	if (command.Equals("quit", StringComparison.OrdinalIgnoreCase))
+	{
+		break;
+	}
+	else if (command.Equals("shops", StringComparison.OrdinalIgnoreCase))
+	{
+		var dataProvider = new CoffeeShopDataProvider();
+		var coffeeShops = dataProvider.LoadShops();
+		foreach (GitHubKurs.Model.CoffeeShop shop in coffeeShops)
+		{
+			Console.WriteLine(shop);
 
+		}
+	}
 }
